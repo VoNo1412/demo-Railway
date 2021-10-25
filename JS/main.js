1511fed18f9ce4f2cb4ee885508b2e092ed55279
VoNo <xkaioken9x@gmail.com>
2021-10-25 13:50:01 +0700

    upload source code

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+let arr = [1, 2, 3, 4, 5];
+
+function array(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    console.log(sum)
+    return sum;
+}
+
+array(arr);
+
